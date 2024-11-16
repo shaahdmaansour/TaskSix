a8277c6cd7c210568fcb062c816f1690071938c2
Shahd Mansour <shmaansour@gmail.com>
2024-11-16 21:41:09 +0200

    Create testCalculator.py

---
+++ testCalculator.py
@@ -0,0 +1,19 @@
+#test if the calculator is working correctly
+import unittest
+from add import add
+
+class TestAddFunction(unittest.TestCase):
+  def test_add(self):
+    #test using positive numbers only
+    self.assertEqual(add(3, 5), 8)
+    #test using negative numbers only
+    self.assertEqual(add(-2, -4), -6)
+    #test using both positive and negative numbers
+    self.assertEqual(add(7, -3), 4)
+    #test adding one zero number
+    self.assertEqual(add(0, 5), 5)
+    #test adding two zeroes
+    self.assertEqual(add(0, 0), 0)    # Both zero
+
+if __name__ == "__main__":
+  unittest.main()
